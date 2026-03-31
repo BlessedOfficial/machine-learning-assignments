11ffd6bd1da0853fae683f120b038893cd20b788
Blessed Panashe Nyathi <bnyathi80@yahoo.com>
2026-03-31 23:00:51 +0200

    Update main.py, add chaining script and input file

--- task 1/main.py
+++ task 1/main.py
@@ -1,26 +1,9 @@
-import os
-from dotenv import load_dotenv
-from openrouter import OpenRouter
+
+from chaining import generate_response
 
 
-load_dotenv()  
+with open("input.txt", "r") as file:
+    user_input = file.read()
+response = generate_response(user_input)
 
-# Get the API key
-api_key = os.getenv("OPENROUTER_API_KEY")
-print("API key loaded:", api_key is not None)
-
-if not api_key:
-    raise ValueError("API key not found. Make sure .env has OPENROUTER_API_KEY set.")
-
-# Create OpenRouter client
-client = OpenRouter(api_key=api_key)
-
-# Send a test request
-response = client.chat.send(
-    model="openrouter/free",
-    messages=[{"role": "user", "content": "Write a one-sentence bedtime story about a unicorn."}]
-)
-
-# Print results
-print(response.choices[0].message.content)
-print("Used model:", response.model)
+print(response)
